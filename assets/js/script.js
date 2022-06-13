17a32d68be20316298ef0dee75bf9d3334267190
RichardMartinAUS <richard.martin001@gmail.com>
2022-06-13 17:30:25 +1000

    reverted some changes to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,51 @@
+var highScores = document.querySelector(".high-scores");
+var gameTimer = document.querySelector(".time");
 var startButton = document.querySelector(".start-button");
-var timeEl = document.querySelector(".time");
-var secondsLeft = 75;
 
-function startTime() {
-  // Sets interval in variable
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = "Time: " + secondsLeft;
-    startButton.disabled = true;
+// var win = document.querySelector(".win");
+// var lose = document.querySelector(".lose");
 
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      sendMessage();
-    }
+var scoreCounter;
+var timerCount;
+var timer;
+// var isWin = false;
 
-  }, 1000);
+// Attach event listener to start button to call startGame function on click
+startButton.addEventListener("click", startGame);
+
+// START GAME FUNCTION
+function startGame() {
+    timerCount = 75;
+    startTimer();
+    renderQuestion();
+    
 }
 
-// Function to create and append colorsplosion image
-function sendMessage() {
-  timeEl.textContent = " ";
-  window.alert("Times Up!");
+// QUESTION PAGES
+function renderQuestion() {
+    var questionText = document.getElementsByClassName(".question");
+    var answerOneText = document.getElementsByClassName(".answer-button-1");
+    var answerTwoText = document.getElementsByClassName(".answer-button-2");
+    var answerThreeText = document.getElementsByClassName(".answer-button-3");
+    var answerFourText = document.getElementsByClassName(".answer-button-4");
+    var startButtonVisibility = document.getElementsByClassName(".start-button");
+
+    startButtonVisibility.style.display = "none";
+    questionText.appendChildren[0].textContent = "This is the first question in a series of questions?";
 }
 
-startButton.addEventListener("click", startTime);
+// The setTimer function starts and stops the timer and triggers loseGame()
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      gameTimer.textContent = "Time left: " + timerCount;
+      startButton.disabled = true;
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        // endGame();
+      }
+    }, 1000);
+}
