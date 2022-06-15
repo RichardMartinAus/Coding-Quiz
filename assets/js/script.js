a381c46e271c6a686c0aa8c297e48b63842f33a4
RichardMartinAUS <richard.martin001@gmail.com>
2022-06-15 15:21:22 +1000

    added eventlistener to answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,13 @@ var highScores = document.querySelector(".high-scores");
 var gameTimer = document.querySelector(".time");
 var startButton = document.querySelector(".start-button");
 var pText = document.getElementById("copyForFirstPage");
+var answerButtons = document.getElementById("answerButtons");
+var previousResult = document.getElementById("previousResult");
 
 // var win = document.querySelector(".win");
 // var lose = document.querySelector(".lose");
 
-var scoreCounter;
+var scoreCounter = 0;
 var timerCount;
 var timer;
 // var isWin = false;
@@ -21,78 +23,67 @@ function startGame() {
     renderQuestion1();
 }
 
+// The setTimer function starts and stops the timer and triggers endGame()
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    gameTimer.textContent = "Time left: " + timerCount;
+    startButton.disabled = true;
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      // endGame();
+    }
+  }, 1000);
+}
+
 // QUESTION PAGES
 function renderQuestion1() {
-    // Hide the start button and P text
-    startButton.style.display = "none";
-    pText.style.display = "none";
-    
-    // Render the question
-    document.getElementById("question").innerHTML = "What operator is used to assign a value to a declared variable?";
-    document.getElementById("question").style.color = "black";
-    document.getElementById("question").style.textAlign = "center";
-    document.getElementById("question").style.fontSize = "2.5rem";
-    document.getElementById("question").style.lineHeight = "2.5rem";
-    document.getElementById("question").style.paddingBottom = "2.5rem";
+  // Hide the start button and P text
+  startButton.style.display = "none";
+  pText.style.display = "none";
+      
+  // Render the question
+  document.getElementById("question").innerHTML = "What operator is used to assign a value to a declare variable?";
+  document.getElementById("question").style.color = "black";
+  document.getElementById("question").style.textAlign = "center";
+  document.getElementById("question").style.fontSize = "2.5rem";
+  document.getElementById("question").style.lineHeight = "2.5rem";
+  document.getElementById("question").style.paddingBottom = "2.5rem";
 
-    // Render the answer buttons
-    // Answer 1
-    document.getElementById("answer-button-1").style.display = "flex"
-    document.getElementById("answer-button-1").innerHTML = "Equal sign (=)"
-    // Answer 2
-    document.getElementById("answer-button-2").style.display = "flex"
-    document.getElementById("answer-button-2").innerHTML = "Question mark (?)"
-    // Answer 3
-    document.getElementById("answer-button-3").style.display = "flex"
-    document.getElementById("answer-button-3").innerHTML = "Double-equal (==)"
-    // Answer 4
-    document.getElementById("answer-button-4").style.display = "flex"
-    document.getElementById("answer-button-4").innerHTML = "Colon (:)"
+  // Render the answer buttons
+  // Answer 1
+  document.getElementById("answer-button-1").style.display = "flex";
+  document.getElementById("answer-button-1").innerHTML = "Equal sign (=)";
+  // Answer 2
+  document.getElementById("answer-button-2").style.display = "flex";
+  document.getElementById("answer-button-2").innerHTML = "Question mark (?)";
+  // Answer 3
+  document.getElementById("answer-button-3").style.display = "flex";
+  document.getElementById("answer-button-3").innerHTML = "Double-equal (==)";
+  // Answer 4
+  document.getElementById("answer-button-4").style.display = "flex";
+  document.getElementById("answer-button-4").innerHTML = "Colon (:)";
 
-    // ADD eventlistener to target
+  var correctAnswer = document.getElementById("answer-button-1");
 
+  // ADD eventlistener to target
+
+  answerButtons.addEventListener("click", function(event) {
+    answer = event.target;
+    console.log(answer);
+
+  if (answer == correctAnswer) {
+    scoreCounter = scoreCounter + 10,
+    console.log(scoreCounter),
+    previousResult.style.display = "block",
+    previousResult.innerHTML = "Correct!";
+  } else if (answer != correctAnswer) {
+    timerCount = timerCount -10,
+    previousResult.style.display = "block",
+    previousResult.innerHTML = "Wrong!";
+    };
+  });
 }
-
-// The setTimer function starts and stops the timer and triggers loseGame()
-function startTimer() {
-    // Sets timer
-    timer = setInterval(function() {
-      timerCount--;
-      gameTimer.textContent = "Time left: " + timerCount;
-      startButton.disabled = true;
-      // Tests if time has run out
-      if (timerCount === 0) {
-        // Clears interval
-        clearInterval(timer);
-        // endGame();
-      }
-    }, 1000);
-}
-
-
-
-// var container = document.querySelector(".container");
-
-// container.addEventListener("click", function(event) {
-//   var element = event.target;
-
-//   if (element.matches(".box")) {
-//     var state = element.getAttribute("data-state");
-
-//     // Use an if statement to conditionally render the number on the card
-//     if (state === "hidden") {
-//       // If the card is clicked while the state is "hidden", we set .textContent to the number 
-//       element.textContent = element.dataset.number;
-//       // Using the dataset property, we change the state to visible because the user can now see the number
-//       element.dataset.state = "visible";
-   
-//     } else {
-//       // 'Hide' the number by setting .textContent to an empty string
-//       element.textContent= "";
-//       // Use .setAttribute() method
-//       element.setAttribute("data-state", "hidden")
-     
-//     }  
-//   }
-  
-// });
